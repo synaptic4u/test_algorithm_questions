06cf55f6e253210a14ddddda142f30a0f61cd5ea
Emile De Wilde <synaptic4u@gmail.com>
2024-10-29 20:37:23 +0200

    making my sets properly

--- intersection.js
+++ intersection.js
@@ -1,6 +1,13 @@
 const intersection = {
   'result': new Set(),
   'find': function(args){
+
+    let setA = new Set((args[0]).split(","));
+    let setB = new Set((args[1]).split(","));
+
+    console.log(setA);
+    console.log(setB);
+
     this.result = args;
 
     return this.result;
