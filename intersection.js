9bffebbb6f4eb3f7ffcf78406e33f27e327cf60a
Emile <emileinitdaemon@gmail.com>
2024-10-30 08:07:54 +0200

    Update intersection.js
    commenting

--- intersection.js
+++ intersection.js
@@ -2,12 +2,14 @@ const intersection = {
   'result': new Set(),
   'find': function(args){
 
+    (/Create sets taking each element of the array which is a string and then split the string into an array which is passed to the Set constrictor elemanating duplicate numbers and white space.
     let setA = new Set((args[0]).split(","));
     let setB = new Set((args[1]).split(","));
 
     console.log(setA);
     console.log(setB);
 
+     //Parses one set using the contains method of the other set.
     foreach(const element of setA){
       if(setA.contains(element)){
         this.result.push(element);
